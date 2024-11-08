2a4de09f7a96d00fddcacefd286155b5434ff6f9
tnantaki <tinyhave@gmail.com>
2024-11-08 16:31:57 +0700

    Build: New Issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       >
         <Theme appearance="dark">
           <NavBar />
-          <main>{children}</main>
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
